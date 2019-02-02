085a51d77ce5d9786673e59ac5c944db7a9a07db
Zachary McGee <zachary.mcgee@siu.edu>
2019-02-01 23:17:19 -0600

    update

--- src/cypher/feistel_block.java
+++ src/cypher/feistel_block.java
@@ -1,72 +1,89 @@
 package cypher;
-
-import java.nio.charset.Charset;
 import java.util.Arrays;
 
 public class feistel_block {
-	public static int rounds = 14;
-	public final static int key = 8;
-	public final static String mkey = "HELLOWORLD";
-	private static final Charset UTF_8 = Charset.forName("UTF-8");
+	final static int ROUNDS = 16;
+	final static int BLOCK_SIZE = 32;
+	
 	public static void main(String args[]) {
-		encrypt("HEYYYYY");
+		String message = "Here's a Message";
+		String messageEnc = encrypt(message);
+		System.out.println(messageEnc);
+		String messageDec = decrypt(messageEnc);
+		System.out.println(hexToAscii(messageDec));
 	}
+	
+	private static char function(char c, char[] key, int i) {
+	    return (char) (c + (key[i % key.length]));    
+	}
+	
+	public static String encrypt(String s) {
+		char[] KEY = {'T', 'H', 'I', 'S', 'I', 'S', 'A', 'K', 'E', 'Y', '1', '2', '3'};
 
-	public static void encrypt(String s) {
-		s = toBinary(s);
-		char[] charArray = s.toCharArray();
-		for(int i = 0; i < (s.length() / 32); i++) {
-			split(s.substring(i * 32, (i+1) * 32));
-		}
-	}
-	
-	public static String toBinary(String s) {
-		byte[] bytes = s.getBytes(UTF_8);
-		  StringBuilder binary = new StringBuilder();
-		  for (byte b : bytes)
-		  {
-		     int val = b;
-		     for (int i = 0; i < 8; i++)
-		     {
-		        binary.append((val & 128) == 0 ? 0 : 1);
-		        val <<= 1;
-		     }
-		  }
-		  s = binary.toString();
-		 return s;
-	}
-	
-	public static String split(String s) {
+		s = stringToHex(s);
 		String leftStr = s.substring(0, s.length() / 2);
-		String rightStr = s.substring(s.length() / 2, s.length());
-		
-	    int[] left = new int[leftStr.length()];
-	    int[] right = new int[rightStr.length()];
-	    
-	    for (int i = 0; i < leftStr.length(); i++) {
-
-	    }
-	    System.out.println ("=====RIGHT=====");
-	    for(int r = 1; r <= rounds; r++) {
-	        for(int i = 0; i < right.length; i++){
-	            left[i] ^= (scramble(right[i], key, r));
-	        }
-	        System.out.println ("ENC"+r +" "+Arrays.toString(left) +" "+Arrays.toString(right));
-	        int[] temp = left; 
-	        left = right; 
-	        right = temp; // swap after
-	    }
-	    for(int r = rounds; r >= 1; r--) {
-	        int[] temp = left; left = right; right = temp; // swap before on decrypt
-	        for(int i = 0; i < right.length; i++) {
-	            left[i] ^= (scramble(right[i], key, r));
-	        }
-	        System.out.println ("DEC"+r + " "+Arrays.toString(left) +" "+Arrays.toString(right));
-	    }
-		return s;
+		String rightStr = s.substring((s.length() / 2), s.length());
+    
+		char[] left = leftStr.toCharArray();
+		char[] right = rightStr.toCharArray();
+		char[] temp;
+    
+		System.out.println(Arrays.toString(left) + "" + Arrays.toString(right));
+		for(int r = 1; r <= ROUNDS; r++) {
+			for(int i = 0; i < right.length; i++){
+				left[i] ^= (function(right[i], KEY, r));
+			}
+			System.out.println ("Encryption Round: " + r + " - " + Arrays.toString(left) + " " + Arrays.toString(right));
+			temp = left; 
+			left = right; 
+			right = temp; 
+		}
+		String finished = String.valueOf(left) + String.valueOf(right);
+		return finished;
 	}
 	
-	private static int scramble(int character, int key, int roundNumber) {
-	    return (int) Math.pow(6 * roundNumber * key, character) % 15;
+	public static String decrypt(String s) {
+		char[] KEY = {'T', 'H', 'I', 'S', 'I', 'S', 'A', 'K', 'E', 'Y', '1', '2', '3'};
+		String leftStr = s.substring(0, s.length() / 2);
+		String rightStr = s.substring((s.length() / 2), s.length());
+    
+		char[] left = leftStr.toCharArray();
+		char[] right = rightStr.toCharArray();
+		char[] temp;
+		
+		for(int r = ROUNDS; r >= 1; r--) {
+			temp = left; 
+			left = right; 
+			right = temp;
+			for(int i = 0; i < right.length; i++) {
+				left[i] ^= (function(right[i], KEY, r));
+			}
+			System.out.println ("Decryption Round: " + r + " - " + Arrays.toString(left) + " " + Arrays.toString(right));
+		}
+		String finished = String.valueOf(left) + String.valueOf(right);
+		return finished;
+	}
+	
+	public static String stringToHex(String str){  
+		char[] chars = str.toCharArray();
+		  
+		StringBuffer hex = new StringBuffer();
+		for(int i = 0; i < chars.length; i++){
+		  hex.append(Integer.toHexString((int)chars[i]));
+		}
+		  
+		return hex.toString();
+	}
+	  
+	private static String hexToAscii(String hexStr) {
+		StringBuilder output = new StringBuilder("");
+		    
+		for (int i = 0; i < hexStr.length(); i += 2) {
+			if(i + 1 < hexStr.length()){
+				String str = hexStr.substring(i, i + 2);
+				output.append((char) Integer.parseInt(str, 16));
+			}
+		}
+		return output.toString();
 	}
 }
--- src/cypher/otp.java
+++ src/cypher/otp.java
@@ -9,7 +9,7 @@ public class otp {
 		String message = "Hello World";
 		String message2 = "the program";
 		String key = "supersecret";
-		String testWord = "sowhat";
+		String testWord = "please";
 		String messageHex = stringToHex(message);
 		String message2Hex = stringToHex(message2);
 		String testwordHex = stringToHex(testWord);
@@ -18,38 +18,51 @@ public class otp {
 		//String xorMessage1 = xorHex(messageHex, keyHex);
 		//String xorMessage2 = xorHex(message2Hex, keyHex);
 		
-		String messageee1 = stringToHex("kcilwhzvsba");
-		String messageee2 = stringToHex("lvuwegrwwla");
-		String xorMessage1 = xorHex(messageee1, keyHex);
-		String xorMessage2 = xorHex(messageee2, keyHex);
+		//String messageee1 = stringToHex("kcilwhzvsba");
+		//String messageee2 = stringToHex("uozckikhdet");
+		//String hexMessage1 = xorHex(messageee1, keyHex);
+		//String hexMessage2 = xorHex(messageee2, keyHex);
 
 		
-		//String xorMessage1 = "16840c56b2ec1560c206c101d89adbd808ce90a2fce4842f335a91297d";
-		//String xorMessage2 = "16840005fffd0e61801ad6039a81dfc608ce90a8fce48460604b912870";
+		String xorMessage1 = "16840c56b2ec1560c206c101d89adbd808ce90a2fce4842f335a91297d";
+		String xorMessage2 = "16840005fffd0e61801ad6039a81dfc608ce90a8fce48460604b912870";
+		String xorMessage3 = "04831705f3bc147fc509cd0d94cdc8ce5fd883bffcf68432334b983377";
+		String xorMessage4 = "0f890051b2f5092fcd1384039e8bd3c84d999eb5fcf68e22334c9922700";
+		String xorMessage5 = "118d1c05e6f4022fd705d608d89ddbd85bd09fafb9e28d25615a9e3961";
+		String xorMessage6 = "0f954551fbf1022fc11e841f91989ac349cad1b9b9f58560744d953b70";
 		
-		System.out.println(xorMessage1);
-		System.out.println(xorMessage2);
 		
-		String xorBoth = xorHex(xorMessage1, xorMessage2);
-		
-		System.out.println(xorBoth);
-		
-		System.out.println(testwordHex.length());
-		System.out.println(xorBoth.length());
-		
-		System.out.println(xorBoth);
+		String[] xorBoth = new String[16];
+		xorBoth[0] = "0" + xorHex(xorMessage1, xorMessage2);
+		xorBoth[1] = "0" + xorHex(xorMessage1, xorMessage3);
+		xorBoth[2] = "0" + xorHex(xorMessage1, xorMessage4);
+		xorBoth[3] = "0" + xorHex(xorMessage1, xorMessage5);
+		xorBoth[4] = "0" + xorHex(xorMessage1, xorMessage6);
+		xorBoth[5] = "0" + xorHex(xorMessage2, xorMessage3);
+		xorBoth[6] = "0" + xorHex(xorMessage2, xorMessage4);
+		xorBoth[7] = "0" + xorHex(xorMessage2, xorMessage5);
+		xorBoth[8] = "0" + xorHex(xorMessage2, xorMessage6);
+		xorBoth[9] = "0" + xorHex(xorMessage3, xorMessage4);
+		xorBoth[10] = "0" + xorHex(xorMessage3, xorMessage5);
+		xorBoth[11] = "0" + xorHex(xorMessage3, xorMessage6);
+	    xorBoth[12] = "0" + xorHex(xorMessage4, xorMessage5);
+		xorBoth[13] = "0" + xorHex(xorMessage4, xorMessage6);
+		xorBoth[14] = "0" + xorHex(xorMessage5, xorMessage6);
 
-		for(int i = 0; i < xorBoth.length(); i++) {
-				String xorTrim = xorBoth.substring(i, i + testwordHex.length());
+		for(int j = 0; j < 14; j++) {
+			System.out.println("ATTEMPT #" + j + "@@@@@@@@@@@");
+			for(int i = 0; i < xorBoth[j].length() - 16; i++) {
+				String xorTrim = xorBoth[j].substring(i, i + testwordHex.length());
 				String result = xorHex(xorTrim, testwordHex);
-				for(int j = 0; j < i; j++) {
+				for(int k = 0; k < i; k++) {
 					System.out.print(".");
 				}
 				System.out.print(hexToAscii(result));
-				for(int j = 0; j < xorBoth.length() - i; j++) {
+				for(int k = 0; k < xorBoth[j].length() - i; k++) {
 					System.out.print(".");
 				}
 				System.out.println();
+		}
 		}
 		
 	}
